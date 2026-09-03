3c7dd178f0fa635cc7f7ff17dfc55046a748d340
BEATRIZ JULIÃO BOLETINI DA SILVA <beatriz.silva462@senaisp.edu.br>
2026-09-02 22:14:25 -0300

    camada da prata

--- t_trat_cred_tgt.py
+++ t_trat_cred_tgt.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import os
+
+# pasta de saída da Camada Silver
+pasta_tgt = './tgt'
+
+# cria a pasta caso ela não exista
+os.makedirs(pasta_tgt, exist_ok=True)
+
 
 # carregar os dados brutos da Camada Bronze
 inad = pd.read_csv('./csl/t_cred_inad_csl.csv')
@@ -8,31 +16,39 @@ comp = pd.read_csv('./csl/t_cred_comp_csl.csv')
 inad = inad.drop_duplicates()
 comp = comp.drop_duplicates()
 
-# convertr período para data para realizar as operações temporais
+# converter período para data para realizar as operações temporais
 inad['periodo'] = pd.to_datetime(inad['periodo'])
 comp['periodo'] = pd.to_datetime(comp['periodo'])
 
-# corrigir outlier de Jan/2022 na inadimplência (ajustando de 293% para o real de 3.3%)
+# corrigir outlier de Jan/2022 na inadimplência
 inad.loc[inad['periodo'] == '2022-01-01', 'inadimplencia_pf_bruto'] = 3.3
 
-#renomear as colunas (removendo o sufixo '_bruto' da camada anterior)
-inad = inad.rename(columns={'inadimplencia_pf_bruto': 'inadimplencia_pf'})
+# renomear as colunas
+inad = inad.rename(columns={
+    'inadimplencia_pf_bruto': 'inadimplencia_pf'
+})
+
 comp = comp.rename(columns={
     'comprometimento_total_bruto': 'comprometimento_total',
     'comprometimento_sem_imob_bruto': 'comprometimento_sem_imob'
 })
 
-# uniao das tabelas em uma única base Silver consolidada (INNER JOIN)
-consolidado = pd.merge(inad, comp, on='periodo', how='inner').sort_values('periodo')
+# união das tabelas em uma única base Silver consolidada
+consolidado = pd.merge(
+    inad,
+    comp,
+    on='periodo',
+    how='inner'
+).sort_values('periodo')
 
-# formata as datas de volta para texto no padrão internacional (AAAA-MM-DD)
+# formatar as datas de volta para texto
 inad['periodo'] = inad['periodo'].dt.strftime('%Y-%m-%d')
 comp['periodo'] = comp['periodo'].dt.strftime('%Y-%m-%d')
 consolidado['periodo'] = consolidado['periodo'].dt.strftime('%Y-%m-%d')
 
-# salva arquivos saneados da Camada Silver
-inad.to_csv('t_cred_inad_tgt.csv', index=False)
-comp.to_csv('t_cred_comp_tgt.csv', index=False)
-consolidado.to_csv('t_cred_cons_tgt.csv', index=False)
+# salvar arquivos saneados da Camada Silver
+inad.to_csv(f'{pasta_tgt}/t_cred_inad_tgt.csv', index=False)
+comp.to_csv(f'{pasta_tgt}/t_cred_comp_tgt.csv', index=False)
+consolidado.to_csv(f'{pasta_tgt}/t_cred_cons_tgt.csv', index=False)
 
 print("Camada Silver processada e salva com sucesso")
